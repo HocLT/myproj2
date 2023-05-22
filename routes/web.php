c4f7e023e09d112f303c7eb0f4f83fb3fa08d445
HocLT <hdquang6@gmail.com>
2023-05-22 10:31:54 +0700

    u1

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin', [DashboardController::class, 'home'])->name('admin');
+
+Route::resource('/product', ProductController::class);
